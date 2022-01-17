90ec034aa8351949ce0f8e85a31f52d9867772bf
Gabriel Gomes de Siqueira <gsiqueira@atena.ic.unicamp.br>
2022-01-17 17:01:09 +0000

    Created global main function

---
+++ __main__.py
@@ -0,0 +1,9 @@
+from dac import extract_dac
+
+def main():
+    extract_dac.main()    
+    # TODO: create EXTRACT_COMVEST, EXTRACT_RAIS and REPLICATE_IDS func
+
+# TODO: implement verbose option 
+if __name__ == '__main__':
+    main()
