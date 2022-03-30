2b71c52d153732740e70edc4b324c86b07448cc1
Fernando Santos de Souza <fernando@atena.ic.unicamp.br>
2022-03-30 18:23:38 +0000

    wrong and corect dataframes

--- dac/clr_vida_academica_habilitacao/__main__.py
+++ dac/clr_vida_academica_habilitacao/__main__.py
@@ -9,9 +9,18 @@ RESULT_NAME = 'vida_academica_habilitacao.csv'
 def main():
     habilitacoes_df = cursos_habilitacoes.generate_clean_data()
     vida_habilitacao_df = vida_academica_habilitacao.generate()       
-    merged_df = pd.merge(vida_habilitacao_df, habilitacoes_df, on=['curso', 'ano_ingresso', 'codigo_habilitacao'], how='outer')
-    #write_result(merged_df, RESULT_NAME)
-    write_output(merged_df, RESULT_NAME)
+    merged_df = pd.merge(vida_habilitacao_df, habilitacoes_df, on=['curso', 'ano_ingresso', 'codigo_habilitacao'], how='left')
+    
+    filtro = merged_df['total_creditos_curso_hab'].notnull()
 
+    corect_merge = merged_df[filtro]
+    wrong_merge = merged_df[~filtro]
+
+    print(corect_merge.shape)
+    print(wrong_shape.shape)
+
+    write_result(corect, RESULT_NAME)
+    write_output(wrong, RESULT_NAME)
+    
 if __name__ == '__main__':
     main()
