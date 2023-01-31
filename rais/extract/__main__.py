9996a5eab0491861a331541e4393e21046a3acff
Luis Felipe <lf_coutinho22@hotmail.com>
2023-01-31 02:18:38 +0000

    Modifica funcoes de padronizacao

--- rais/extract/__main__.py
+++ rais/extract/__main__.py
@@ -4,11 +4,13 @@ from rais.extract import merge
 from rais.extract import recover_cpf_rais
 from rais.extract import clear
 
+
 def clear_databse():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     merge.merge_all_years()
     recover_cpf_rais.recover_cpf_all_years()
     clear.clear_all_years()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     clear_databse()
