3f80044ec0758774a050da97aeef1964cad599c2
Luis Felipe <lf_coutinho22@hotmail.com>
2023-02-07 01:36:08 +0000

    Remove funcoes nao utilizadas

--- socio/cleaning/clear.py
+++ socio/cleaning/clear.py
@@ -1,24 +1,14 @@
-from socio.database_information.estabelecimento import get_columns_info_estabelecimento
 from socio.database_information.socio import get_columns_info_socio
-from socio.database_information.empresa import get_columns_info_empresa
 
 from socio.utilities.io import (
-    read_estabelecimento_original_novolayout,
     read_socio_original,
-    write_estabelecimento_tmp,
 )
-from socio.utilities.io import read_empresa_original
-from socio.utilities.io import read_cnae_original
 
 from socio.utilities.io import write_socio_tmp
-from socio.utilities.io import write_empresa
-from socio.utilities.io import write_cnae
 
 from socio.utilities.io import create_folder_socio_tmp_date
 from socio.utilities.io import create_folder_socio_tmp
-from socio.utilities.io import create_folder_empresa_tmp_date
 from socio.utilities.io import list_dirs_socio_input
-from socio.utilities.io import list_dirs_empresa_input
 from socio.utilities.io import get_all_files
 
 from socio.utilities.logging import log_cleaning_database
@@ -34,45 +24,6 @@ def clear_socio():
 
     for folder in socio_folders:
         clear_date_socio(folder)
-
-
-def clear_cnae_secundaria():
-    log_cleaning_database("CNAE")
-    df = read_cnae_original()
-    write_cnae(df)
-
-
-# ------------------------------------------------------------------------------------------------
-def clear_empresa():
-    log_cleaning_database("Empresa")
-    date = path.split("/")[-1]
-    date_clean = date.replace("-", "")
-
-    create_folder_empresa_tmp_date(date)
-    files = get_all_files(path)
-    for file in files:
-        filename = file.split("/")[-1]
-        log_cleaning_file(path, filename)
-        df = read_empresa_original(file)
-
-
-# Usado para os arquivos a partir de 2022, eles tem um novo layout
-def clear_estabelecimento():
-    date = path.split("/")[-1]
-    date_clean = date.replace("-", "")
-
-    create_folder_empresa_tmp_date(date)
-    files = get_all_files(path)
-    for file in files:
-        filename = file.split("/")[-1]
-        log_cleaning_file(path, filename)
-        df = read_estabelecimento_original_novolayout(file)
-
-        df["data_coleta"] = date_clean
-
-        df = clear_file_estabelecimento(df)
-
-        write_estabelecimento_tmp(df, filename, date)
 
 
 def clear_date_socio(path):
@@ -96,11 +47,6 @@ def clear_date_socio(path):
 
 def clear_file_socio(df):
     columns_info = get_columns_info_socio()
-    return clear_columns(df, columns_info)
-
-
-def clear_file_estabelecimento(df):
-    columns_info = get_columns_info_estabelecimento()
     return clear_columns(df, columns_info)
 
 
