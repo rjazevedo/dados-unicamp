a97290c35cc16c7d6ca9f701418537d589afb836
Luis Felipe <lf_coutinho22@hotmail.com>
2023-02-07 01:35:45 +0000

    Remove funcoes nao utilizadas

--- socio/cleaning/__main__.py
+++ socio/cleaning/__main__.py
@@ -5,11 +5,6 @@ import logging
 def pre_process():
     logging.basicConfig(level=logging.INFO)
     clear.clear_socio()
-    # clear.clear_empresa()
-    # clear.clear_cnae_secundaria()
-    # clear_date_estabelecimento(
-    #     "/home/luisfelipe/dados-unicamp/input/empresa/2022-03-12"
-    # )
 
 
 if __name__ == "__main__":
