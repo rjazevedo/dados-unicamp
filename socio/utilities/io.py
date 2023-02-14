be1a6236c3ba0cdfbcb4ea78b2fcf250a46524f9
Luis Felipe <lf_coutinho22@hotmail.com>
2023-02-14 18:34:06 +0000

    Remove codigo inutilizado

--- socio/utilities/io.py
+++ socio/utilities/io.py
@@ -7,9 +7,6 @@ import subprocess
 from socio.utilities.dtype import get_dtype
 
 from socio.database_information.socio import get_columns_info_socio
-from socio.database_information.empresa import get_columns_info_empresa
-from socio.database_information.estabelecimento import get_columns_info_estabelecimento
-from socio.database_information.cnae_secundaria import get_columns_info_cnae_secundaria
 
 stream = open("socio/configuration.yaml")
 config = yaml.safe_load(stream)
@@ -17,12 +14,11 @@ config = yaml.safe_load(stream)
 
 def read_ids():
     file = config["database_ids"]
-    dtype = {"nome": "object", "cpf": "object", "id": "Int64"}
+    dtype = {"nome": "string", "cpf": "string", "id": "Int64"}
     df = pd.read_csv(file, dtype=dtype, sep=",", low_memory=False)
     return df
 
 
-# ------------------------------------------------------------------------------------------------
 def read_socio_original(path, date):
     columns_info = get_columns_info_socio()
 
@@ -48,79 +44,12 @@ def read_socio_original(path, date):
         return read_database(path, columns_info)
 
 
-def read_estabelecimento_original_novolayout(file):
-    columns_info = get_columns_info_estabelecimento()
-    dtype = get_dtype(columns_info, True)
-    columns_names = list(columns_info)
-    df = pd.read_csv(
-        file,
-        names=columns_names,
-        dtype=dtype,
-        low_memory=False,
-        sep=";",
-        encoding="latin",
-        warn_bad_lines=True,
-        error_bad_lines=False,
-    )
-    return df
-
-
-def read_empresa_original(file):
-    columns_info = get_columns_info_empresa()
-    dtype = get_dtype(columns_info, True)
-    cols = [col for col in columns_info]
-    df = pd.read_csv(
-        file,
-        dtype=dtype,
-        low_memory=False,
-        sep=",",
-        encoding="latin",
-        warn_bad_lines=True,
-        error_bad_lines=False,
-    )
-    return df
-
-
-def read_cnae_original():
-    file = config["database_cnae"]
-    dtype = {0: "object", 1: "object"}
-    df = pd.read_csv(file, dtype=dtype, header=None)
-    df = rename_columns_cnae(df)
-    return df
-
-
-# ------------------------------------------------------------------------------------------------
 def read_socio_clean(path):
     return pd.read_parquet(path)
 
 
-def read_empresa_clean():
-    file = config["path_output"] + "empresa.csv"
-    columns_info = get_columns_info_empresa()
-    df = read_database(file, columns_info, is_original=False)
-    return df
-
-
-def read_cnae_clean():
-    file = config["path_output"] + "cnae_secundaria.csv"
-    columns_info = get_columns_info_cnae_secundaria()
-    df = read_database(file, columns_info, is_original=False)
-    return df
-
-
-# ------------------------------------------------------------------------------------------------
 def write_socio(df):
     file = config["path_output"] + "socio.csv"
-    write_database(df, file)
-
-
-def write_empresa(df):
-    file = config["path_output"] + "empresa.csv"
-    write_database(df, file)
-
-
-def write_cnae(df):
-    file = config["path_output"] + "cnae_secundaria.csv"
     write_database(df, file)
 
 
@@ -167,16 +96,9 @@ def read_database(
         low_memory=low_memory,
         sep=sep,
         encoding=encoding,
-        on_bad_lines='warn',
+        on_bad_lines="warn",
         names=cols,
     )
-    return df
-
-
-def rename_columns_cnae(df):
-    df["cnpj"] = df[0]
-    df["cnae"] = df[1]
-    df = df.loc[:, ["cnpj", "cnae"]]
     return df
 
 
@@ -190,18 +112,8 @@ def create_folder_socio_tmp():
     create_folder(path, "tmp")
 
 
-def create_folder_empresa_tmp():
-    path = config["database_output"]
-    create_folder(path, "tmp")
-
-
 def create_folder_socio_tmp_date(year):
     path = config["path_output"] + "tmp/"
-    create_folder(path, str(year))
-
-
-def create_folder_empresa_tmp_date(year):
-    path = config["database_output"] + "tmp/"
     create_folder(path, str(year))
 
 
@@ -225,10 +137,6 @@ def get_all_files(path):
 
 
 def list_dirs_socio_input():
-    return [f.path for f in os.scandir(config["path_socio"]) if f.is_dir()]
-
-
-def list_dirs_empresa_input():
     return [f.path for f in os.scandir(config["path_socio"]) if f.is_dir()]
 
 
