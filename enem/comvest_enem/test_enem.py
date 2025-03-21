7ad864434ec7a41d623d5c2ae7d1fa0d8bbe43f1
Giovanipt2 <giovanimambrimleme@gmail.com>
2025-03-21 20:25:04 +0000

    Remove clear_fin script and update test file paths for consistency

--- enem/comvest_enem/test_enem.py
+++ enem/comvest_enem/test_enem.py
@@ -10,18 +10,19 @@ def main():
     for year in range(2012, 2021):
         print(f"Comparando o ano {year}") 
         new_file_path = f'/home/output/enem/comvest_enem{year}.csv'
-        old_file_path = f'/home/jonatas/testes-enem/enem/comvest_enem{year}.csv'
+        old_file_path = f'/home/gsiqueira/dados-unicamp/output/comvest_enem{year}.csv'
         
         new_df = pd.read_csv(new_file_path)
         old_df = pd.read_csv(old_file_path)
 
         # Imprimindo quantos valores não-nulos há nas colunas comvest_{year + 1} e comvest_{year + 2}
-        print(f"Quantidade de valores não-nulos nas colunas comvest_{year + 1}:")
-        print("Para o arquivo antigo:")
-        print(old_df[f'comvest_{year + 1}'].count())
-        print("Para o arquivo novo:")
-        print(new_df[f'comvest_{year + 1}'].count())
-        print()
+        if year != 2020:
+            print(f"Quantidade de valores não-nulos nas colunas comvest_{year + 1}:")
+            print("Para o arquivo antigo:")
+            print(old_df[f'comvest_{year + 1}'].count())
+            print("Para o arquivo novo:")
+            print(new_df[f'comvest_{year + 1}'].count())
+            print()
         
         if year != 2019:
             print(f"Quantidade de valores não-nulos nas colunas comvest_{year + 2}:")
